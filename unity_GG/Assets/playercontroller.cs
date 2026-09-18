475f6ed88347d1649f693cf3e36844c5731999d6
박준서 <pjs030505@gmail.com>
2026-09-18 12:32:40 +0900

    3주차

--- unity_GG/Assets/playercontroller.cs
+++ unity_GG/Assets/playercontroller.cs
@@ -3,16 +3,46 @@ using UnityEngine.InputSystem;
  
 public class PlayerController : MonoBehaviour
 {
-  public float moveSpeed = 5f;
+    public string playerName = "쭌서";
+    public int hp = 100;
+    public float moveSpeed = 5f;
+    public Transform visual;
   private Vector2 moveInput;
+    public float jumpPower = 8f;
+    private Rigidbody2D rb;
+
+    public Vector3 startPosition;
+    void Start()
+
+    {
+        rb = GetComponent<Rigidbody2D>();
+        transform.position = startPosition;
+    }
  
   void OnMove(InputValue value)
   {
     moveInput = value.Get<Vector2>();
+        if (moveInput.x > 0)
+        {
+            visual.localScale = new Vector3(1, 1, 1);
+        }
+        else if (moveInput.x < 0)
+        {
+            visual.localScale = new Vector3(-1, 1, 1);
+        }
   }
- 
-  void Update()
+    void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            Debug.Log("점프!");
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+        }
+    }
+
+    void Update()
   {
-    transform.Translate(Vector3.right * moveInput.x * moveSpeed * Time.deltaTime);
+        //Debug.Log("Update");
+        transform.Translate(Vector3.right * moveInput.x * moveSpeed * Time.deltaTime);
   }
 }
